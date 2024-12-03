42d279a7eb178137b8f13dbc48dae1ab9b6cd030
Usama2201 <usamaali191@gmail.com>
2024-12-03 12:30:00 +0500

    Config Routes Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+//   {
+//     path: 'login',
+//     loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+//   },
+//   {
+//     path: 'register',
+//     loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+//   },
+//   {
+//     path: 'dashboard',
+//     loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+//     canActivate: [authGuard]
+//   },
+  { path: '**', redirectTo: 'dashboard' }
+];
