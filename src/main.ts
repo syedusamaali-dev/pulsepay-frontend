d69d35f1f75aeac3c7f8c0054871b2e8b6d049c4
Usama2201 <usamaali191@gmail.com>
2025-01-06 12:30:00 +0500

    Build Fixing

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // <--- ADD THIS LINE FIRST
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
