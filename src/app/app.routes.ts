0ed85f47b32125a2cddf7fbf5a979b3f01019009
Usama2201 <usamaali191@gmail.com>
2025-01-07 12:30:00 +0500

    Updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,18 @@ import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-//   {
-//     path: 'login',
-//     loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
-//   },
-//   {
-//     path: 'register',
-//     loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
-//   },
-//   {
-//     path: 'dashboard',
-//     loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
-//     canActivate: [authGuard]
-//   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.DashboardComponent),
+    canActivate: [authGuard]
+  },
   { path: '**', redirectTo: 'dashboard' }
 ];
