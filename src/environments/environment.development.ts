b141009d685eb9a87ff71f3f35843fc691d64a05
Usama2201 <usamaali191@gmail.com>
2025-01-09 12:30:00 +0500

    Env Change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const devEnv = {
   production: false,
-  apiUrl: 'http://localhost:5050/api',
-  socketUrl: 'http://localhost:5050',
+  apiUrl: 'http://localhost:5000/api',
+  socketUrl: 'http://localhost:5000',
 };
