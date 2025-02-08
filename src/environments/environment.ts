214fcf922c4234c65d57473156465ecc153bd336
Usama2201 <usamaali191@gmail.com>
2025-02-08 13:30:00 +0500

    Enviroment Deployment Vercel 1.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-// environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://pulsepay-t8a2hzun.b4a.run/api',
-  socketUrl: 'https://pulsepay-t8a2hzun.b4a.run'
+  apiUrl: '/api',
+  socketUrl: '/'
 };
